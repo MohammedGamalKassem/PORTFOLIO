39a3d25fd00539f58aca5808918fbda11cdc89e2
MohammedGamalKassem <mohammed.ahmad03@eng-st.cu.edu.eg>
2026-06-15 22:29:08 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.NODE_ENV === 'production' ? '/PORTFOLIO/' : '/'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PORTFOLIO/', 
   plugins: [react()],
+  base,
 })
